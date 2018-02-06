010715fb639ad83f765d459503f1291b04d2ee92
Tomoaki Tsuruta <england0112@gmail.com>
2018-02-05 19:57:22 -0800

    Add Auth component

--- frontend/apollo-and-redux-provider.js
+++ frontend/apollo-and-redux-provider.js
@@ -1,7 +1,37 @@
 import { Provider } from 'react-redux';
 import { ApolloProvider } from 'react-apollo';
+import ApolloClient, { createNetworkInterface } from 'react-apollo';
 
-const apolloAndReduxProviderHOC = (WrappedComponent, store, client) => {
+const uri = '/graphql';
+
+
+const apolloAndReduxProviderHOC = (WrappedComponent, store) => {
+  const networkInterface = createNetworkInterface({
+    uri,
+    opts: {
+           credentials: 'same-origin',
+           mode: 'cors'
+       }
+  });
+  //
+  // networkInterface.use([{
+  //      async applyMiddleware(req, next) {
+  //          try {
+  //              if (!req.options.headers) req.options.headers = {}
+  //              const token = await AsyncStorage.getItem('token')
+  //              req.options.headers.authorization = token || null
+  //              next()
+  //          } catch (error) {
+  //              next()
+  //          }
+  //      }
+  //  }])
+
+  const client = new ApolloClient({
+    networkInterface,
+    dataIdFromObject: r => r.id
+  });
+
   class Enhance extends React.Component {
     render () {
       return (
