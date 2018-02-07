6732bd5779efe3b1f76eec329297bec2024f99cf
Tomoaki Tsuruta <england0112@gmail.com>
2018-02-06 22:10:22 -0800

    Add Facebook SDK

--- frontend/src/screens/Auth/Auth.js
+++ frontend/src/screens/Auth/Auth.js
@@ -3,17 +3,56 @@ import { connect } from 'react-redux';
 import { tryAuth } from "../../store/actions/index"
 
 import {
-  View, Text
+  View,
+  Text,
+  TouchableOpacity
 } from 'react-native';
 
+import FBSDK, { LoginManager, AccessToken } from 'react-native-fbsdk';
+import firebase from 'firebase';
+
 class AuthScreen extends Component {
+  constructor(props) {
+    super(props);
+    console.log('initia')
+  }
+  state = {
+    hoge: 'h'
+  }
+
+  fbLoginHandler = () => {
+    console.log(FBSDK);
+    console.log("aaaaaaaaa")
+    console.log(LoginManager);
+    console.log(this.state.hoge);
+    console.log(LoginManager.logInWithReadPermissions(['email']))
+    LoginManager.logInWithReadPermissions(['email']).then(result => {
+      if (result.isCancelled) {
+        console.log("Login is cancelled")
+      } else {
+        console.log(result)
+        AccessToken.getCurrentAccessToken()
+        .then(accessTokenData => {
+          const credentials = firebase.auth.FacebookAuthProvider.credential(accessTokenData.accessToken);
+          firebase.auth().signInWithCredential(credential)
+          .then(result => {
+                console.log(result)
+          }
+        ).catch(error => console.log(error))
+        })
+        result.grantedPermissions.toString()
+      }
+
+    }).catch(error => console.log(error))
+  }
 
   render() {
-    this.props.onLogin();
-    console.log(this.props.isLoading);
+
     return (
       <View>
-        <Text> Auth Screen</Text>
+        <TouchableOpacity onPress={this.fbLoginHandler}>
+          <Text> Auth Screen</Text>
+        </TouchableOpacity>
       </View>
     )
   }
