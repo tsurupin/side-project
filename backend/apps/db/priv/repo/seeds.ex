dd70249548743d825589e5a29fe5bc3848fc77a0
Tomoaki Tsuruta <england0112@gmail.com>
2018-03-10 21:59:13 -0800

    Add some models

--- backend/apps/db/priv/repo/seeds.ex
+++ backend/apps/db/priv/repo/seeds.ex
@@ -1,8 +1,4 @@
 alias Db.Repo
-alias Db.Accounts
-alias Yummy.Accounts.User
-alias Yummy.Recipes
-alias Yummy.Recipes.Recipe
+alias Db.Users.User
 
 User |> Repo.delete_all
-{:ok, user} = Accounts.create_user(%{name: "Jose", email: "jose@yummy.com", password: "password", password_confirmation: "password"})
