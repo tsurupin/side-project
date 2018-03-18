dfd6af1ae7b9de2d10183851a7acef1f1924077a
Tomoaki Tsuruta <england0112@gmail.com>
2018-03-18 08:23:14 -0700

    Add more schema

--- backend/apps/api/lib/api_web/schema/chat_types.ex
+++ backend/apps/api/lib/api_web/schema/chat_types.ex
@@ -1,6 +1,9 @@
 defmodule ApiWeb.Schema.ChatTypes do
   use Absinthe.Schema.Notation
 
+  alias Db.Chats.Content
+  alias Db.Uploaders.ChatImageUploader
+
   object :chat do
     field :id, :id
     field :name, :string
@@ -13,12 +16,14 @@ defmodule ApiWeb.Schema.ChatTypes do
 
 
   object :chat_content do
-    field :id, :id,
-    field :message, :message,
-    field :primary_image_url, :image_url, resolve: fn(_args, %{source: chat_content}) ->
-      {:ok, chat_content.image_url.primary}
+    field :id, :id
+    field :message, :message
+    field :image_url, :string do
+      arg :format, :string, default_value: "thumb"
+      resolve fn (%{Content{image_url: image_url} = content, %{format: format},_) ->
+        {:ok, ChatImageUploader.url({image_url, content}, String.to_atom(format))}
+      end
     end
-
     field :user, :user
   end
 
