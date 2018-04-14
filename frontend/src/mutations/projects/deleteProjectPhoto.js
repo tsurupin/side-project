5370080b102636f1a4664233719363225f4d5a10
Tomoaki Tsuruta <england0112@gmail.com>
2018-04-13 20:13:07 -0700

    Create DeleteProjectPhoto.js

---
+++ frontend/src/mutations/projects/deleteProjectPhoto.js
@@ -0,0 +1,20 @@
+
+import { graphql } from "react-apollo";
+import type { OperationComponent } from "react-apollo";
+import DELETE_PROJECT_PHOTO from "../../graphql/projects/deleteProjectPhotoMutation.graphql";
+
+
+type InputProps = {
+  photoId: number
+};
+
+const deleteProjectPhoto: OperationComponent<null, InputProps> = graphql(DELETE_PROJECT_PHOTO, {
+  name: "deleteProjectPhoto",
+  options: props => ({
+    variables: {
+      photoId: props.photoId
+    }
+  })
+});
+
+export default deleteProjectPhoto;
---
+++ frontend/src/mutations/projects/index.js
@@ -0,0 +1,7 @@
+import createProject from './createProject';
+import editProject from './editProject';
+
+export {
+  createProject,
+  editProject
+};
