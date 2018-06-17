ef1b034e109b46a9d71546624ccb54273a55e8a6
Tomoaki Tsuruta <england0112@gmail.com>
2018-06-17 16:31:26 -0700

    Create FindOrCreateCityMutation

---
+++ frontend/src/mutations/cities/FindOrCreateCityMutation.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import { FIND_OR_CREATE_CITY_MUTATION } from "../../graphql/cities";
+
+type Props = {
+  children: any;
+};
+
+const FindOrCreateCityMutation = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Mutation mutation={FIND_OR_CREATE_CITY_MUTATION} context={{ needAuth: true }}>
+      {(findOrCreateCityMutation, { loading, error, data }) => {
+        return children({ findOrCreateCityMutation, loading, error, data });
+      }}
+    </Mutation>
+  );
+};
+
+export default FindOrCreateCityMutation;
---
+++ frontend/src/mutations/cities/index.ts
@@ -0,0 +1,4 @@
+import FindOrCreateCityMutation from "./FindOrCreateCityMutation";
+export {
+  FindOrCreateCityMutation
+}
