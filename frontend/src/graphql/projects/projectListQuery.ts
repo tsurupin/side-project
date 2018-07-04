ea9fd627e4765ca83bbc6f9b0ede999e2be6c61c
Tomoaki Tsuruta <england0112@gmail.com>
2018-07-04 16:45:26 -0700

    Add CityId for ProjectSearch

--- frontend/src/graphql/projects/projectListQuery.ts
+++ frontend/src/graphql/projects/projectListQuery.ts
@@ -3,8 +3,8 @@ import { PROJECT_FRAGMENTS } from "./projectFragments";
 import { parseFragment } from "../utilities/parseFragment";
 
 export const PROJECT_LIST_QUERY = gql`
-query Projects($genreId: ID, $skillIds: [ID]) {
-  projects(conditions: {genreId: $genreId, skillIds: $skillIds}) {
+query Projects($genreId: ID, $cityId: ID, $skillIds: [ID]) {
+  projects(conditions: {genreId: $genreId, $cityId: ID, skillIds: $skillIds}) {
     ${parseFragment(PROJECT_FRAGMENTS.projectOnList)}
   }
 }`;
