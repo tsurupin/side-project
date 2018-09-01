58474a4e42e4cedaca58e5faf83814ad5d81001c
Tomoaki Tsuruta <england0112@gmail.com>
2018-08-31 18:53:27 -0700

    Fix EditableProjectList after new product created

--- frontend/src/graphql/projects/createProjectMutation.ts
+++ frontend/src/graphql/projects/createProjectMutation.ts
@@ -5,6 +5,6 @@ import { parseFragment } from "../utilities/parseFragment";
 export const CREATE_PROJECT_MUTATION = gql`
 mutation CreateProject($title: String!, $leadSentence: String, $motivation: String, $requirement: String, $genreId: ID, $skillIds: [ID]) {
   createProject(projectInput: {title: $title, leadSentence: $leadSentence, motivation: $motivation, requirement: $requirement, genreId: $genreId, skillIds: $skillIds}) {
-    ${parseFragment(PROJECT_FRAGMENTS.projectCore)}
+    ${parseFragment(PROJECT_FRAGMENTS.projectOnList)}
   }
 }`;
