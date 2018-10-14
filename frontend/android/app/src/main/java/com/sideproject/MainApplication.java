4adc7b1b11df8c82eacada1517f09e94345e87a1
Tomoaki Tsuruta <england0112@gmail.com>
2018-10-13 22:23:47 -0700

    Set verctor-icons

--- frontend/android/app/src/main/java/com/sideproject/MainApplication.java
+++ frontend/android/app/src/main/java/com/sideproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.sideproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new ImageResizerPackage(),
             new FBSDKPackage()
       );
