dbb27e49dbe4d0e01c52c02599e49cb6ae695e97
Tomoaki Tsuruta <england0112@gmail.com>
2018-10-21 21:35:54 -0700

    Add error handling for optimistic data

--- frontend/src/mutations/projects/CreateProjectMutation.tsx
+++ frontend/src/mutations/projects/CreateProjectMutation.tsx
@@ -18,23 +18,29 @@ const CreateProjectMutation = (props: Props) => {
       context={{ needAuth: true }}
       update={(cache, { data: { createProject } }) => {
         console.log("doc", EDITABLE_PROJECT_LIST_QUERY);
+        // NOTE: reqdQuery cannot return optimistic data yet
+        // https://github.com/apollographql/apollo-feature-requests/issues/14
+        try {
         
-        const projectData = cache.readQuery({
-          query: EDITABLE_PROJECT_LIST_QUERY
-        });
-        
-        const projects = projectData.editableProjects;
-        const newProject = {
-          __typename: "Project",
-          id: createProject.id,
-          ...createProject
-        };
-        const editableProjects = [...projects, newProject];
+          const projectData = cache.readQuery({
+            query: EDITABLE_PROJECT_LIST_QUERY
+          });
+          
+          const projects = projectData.editableProjects;
+          const newProject = {
+            __typename: "Project",
+            id: createProject.id,
+            ...createProject
+          };
+          const editableProjects = [...projects, newProject];
 
-        cache.writeQuery({
-          query: EDITABLE_PROJECT_LIST_QUERY,
-          data: { editableProjects }
-        });
+          cache.writeQuery({
+            query: EDITABLE_PROJECT_LIST_QUERY,
+            data: { editableProjects }
+          });
+        } catch(e) {
+          console.log("error", e)
+        }
       }}
     >
       {(createProjectMutation, { loading, error, data }) => {
