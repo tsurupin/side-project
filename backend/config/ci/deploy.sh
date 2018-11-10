9220478b421ed348af24f1749f7d6653fba72ec8
Tomoaki Tsuruta <england0112@gmail.com>
2018-11-10 14:01:13 -0800

    Fix deploy script

--- backend/config/ci/deploy.sh
+++ backend/config/ci/deploy.sh
@@ -4,11 +4,12 @@
 set -e
 
 # Install AWS CLI
-pip install --user awscli
+# pip install --user awscli
 
 # Install AWS ECS CLI
-sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-sudo chmod +x /usr/local/bin/ecs-cli
+# sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+# echo "$(curl -s https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest.md5) /usr/local/bin/ecs-cli" | md5sum -c -
+# sudo chmod +x /usr/local/bin/ecs-cli
 #sudo apt-get install jq
 
 # Set AWS access keys.
@@ -25,7 +26,7 @@ AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 # it's easy to change for a different project. AWS_ECS_URL should be the
 # base url.
 AWS_ECS_URL=$AWS_ECS_URL
-AWS_ECS_PROJECT_NAME=$AWS_PROJECT_NAME
+AWS_ECS_PROJECT_NAME=$AWS_ECS_PROJECT_NAME
 AWS_ECS_CONTAINER_NAME=$AWS_ECS_CONTAINER_NAME
 AWS_ECS_DOCKER_IMAGE=$AWS_ECS_DOCKER_IMAGE
 AWS_ECS_CLUSTER_NAME=$AWS_ECS_CLUSTER_NAME
@@ -51,34 +52,35 @@ PORT=$PORT
 # Build container.
 # As we did before, but now we are going to build the Docker image that will
 # be pushed to the repository.
-docker build -t $AWS_ECS_CONTAINER_NAME \
-  --build-arg AWS_S3_BUCKET=$AWS_S3_BUCKET \
-  --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  --build-arg AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-  --build-arg PHOENIX_SECRET_KEY_BASE=$PHOENIX_SECRET_KEY_BASE \
-  --build-arg POSTGRES_DB_URL=$POSTGRES_DB_URL \
-  --build-arg POSTGRES_DB_POOL_SIZE=$POSTGRES_DB_POOL_SIZE \
-  --build-arg HOST=$HOST \
-  --build-arg PORT=$PORT \
-  --build-arg FIREBASE_SECRET_PEM_FILE_PATH=$FIREBASE_SECRET_PEM_FILE_PATH \
-  --build-arg FIREBASE_SERVICE_ACCOUNT_EMAIL=$FIREBASE_SERVICE_ACCOUNT_EMAIL \
-  .
+# docker build -t $AWS_ECS_CONTAINER_NAME \
+#   --build-arg AWS_S3_BUCKET=$AWS_S3_BUCKET \
+#   --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+#   --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+#   --build-arg AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+#   --build-arg PHOENIX_SECRET_KEY_BASE=$PHOENIX_SECRET_KEY_BASE \
+#   --build-arg POSTGRES_DB_URL=$POSTGRES_DB_URL \
+#   --build-arg POSTGRES_DB_POOL_SIZE=$POSTGRES_DB_POOL_SIZE \
+#   --build-arg HOST=$HOST \
+#   --build-arg PORT=$PORT \
+#   --build-arg FIREBASE_SECRET_PEM_FILE_PATH=$FIREBASE_SECRET_PEM_FILE_PATH \
+#   --build-arg FIREBASE_SERVICE_ACCOUNT_EMAIL=$FIREBASE_SERVICE_ACCOUNT_EMAIL \
+#   .
 
-# Tag the new Docker image as latest on the ECS Repository.
-docker tag $AWS_ECS_DOCKER_IMAGE "$AWS_ECS_URL"/"$AWS_ECS_DOCKER_IMAGE"
+# # Tag the new Docker image as latest on the ECS Repository.
+# docker tag $AWS_ECS_DOCKER_IMAGE "$AWS_ECS_URL"/"$AWS_ECS_DOCKER_IMAGE"
 
 
 # Login to ECS Repository.
-eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+# eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
 # Upload the Docker image to the ECS Repository.
-docker push "$AWS_ECS_URL"/"$AWS_ECS_DOCKER_IMAGE"
+# docker push "$AWS_ECS_URL"/"$AWS_ECS_DOCKER_IMAGE"
 
 # Configure ECS cluster and AWS_DEFAULT_REGION so we don't have to send it
 # on every command
-ecs-cli configure --cluster=$AWS_ECS_CLUSTER_NAME --region=$AWS_DEFAULT_REGION
 #ecs-cli configure profile --profile-name ecs-deploy-user --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY
+ecs-cli configure --cluster=$AWS_ECS_CLUSTER_NAME --region=$AWS_DEFAULT_REGION
+
 # Build docker-compose.yml with our configuration.
 # Here we are going to replace the docker-compose.yml placeholders with
 # our app's configurations
@@ -111,7 +113,7 @@ fi
 
 # Start new task which will create fresh new task definition as well.
 # This is what brings the application up with the new changes and configurations.
-ecs-cli compose \
+ecs-cli compose --verbose\
   --file config/ci/docker-compose.yml \
   --project-name "$AWS_ECS_PROJECT_NAME" \
-  service up
+  service up # —force-deployment --timeout 10
