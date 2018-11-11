bb85ece539037693408e90923c88d0198ccd703a
Tomoaki Tsuruta <england0112@gmail.com>
2018-11-11 15:19:38 -0800

    Fix indent

--- backend/config/ci/deploy.sh
+++ backend/config/ci/deploy.sh
@@ -10,7 +10,7 @@ pip install --user awscli
 sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
 #echo "$(curl -s https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest.md5) /usr/local/bin/ecs-cli" | md5sum -c -
 sudo chmod +x /usr/local/bin/ecs-cli
-sudo apt-get install jq
+
 
 # Set AWS access keys.
 # This is required so that both aws-cli and ecs-cli can access you account
