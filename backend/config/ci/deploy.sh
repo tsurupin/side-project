a5229c96f73b089e93f670b05f7372650b6d6bac
Tomoaki Tsuruta <england0112@gmail.com>
2018-11-23 08:57:18 -0800

    Fix circle.ci script

--- backend/config/ci/deploy.sh
+++ backend/config/ci/deploy.sh
@@ -4,11 +4,11 @@
 set -e
 
 # Install AWS CLI
-# pip install --user awscli
+pip install --user awscli
 
 # # Install AWS ECS CLI
-#sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
-#sudo chmod +x /usr/local/bin/ecs-cli
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
 
 
 # Set AWS access keys.
