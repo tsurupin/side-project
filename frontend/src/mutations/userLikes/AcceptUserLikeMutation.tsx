21d3d332f8846444b95b3380b8f78bdc7551de83
Tomoaki Tsuruta <england0112@gmail.com>
2018-12-02 09:44:50 -0800

    Fix file names

--- frontend/src/mutations/userLikes/AcceptUserLikeMutation.tsx
+++ frontend/src/mutations/userLikes/AcceptUserLikeMutation.tsx
@@ -15,7 +15,6 @@ const AcceptUserLikeMutation = (props: Props) => {
       mutation={ACCEPT_USER_LIKE_MUTATION}
       context={{ needAuth: true }}
       update={(cache, { data: { acceptUserLike: chat } }) => {
-        console.log("ACCEPT_USER_LIKE", chat);
         const { matchList } = cache.readQuery({ query: MATCH_LIST_QUERY });
 
         cache.writeQuery({
